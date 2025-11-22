dda1574fc1c0134fce17c6d6d0ac994c8906cbe4
Kamil Chmielewski <45183584+ChmielewskiKamil@users.noreply.github.com>
2025-11-22 19:25:02 +0100

    2 step parsing: 1. Dispatch/Route 2. Parse + Send response

--- lsp/dispatcher.c
+++ lsp/dispatcher.c
@@ -1,16 +1,43 @@
-#include <dispatcher.h>
+#include "dispatcher.h"
 
-#include "string.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+/////// LSP REQUEST MESSAGE HANDLERS - FORWARD DECLARATIONS ///////
+
+void handle_initialize(int32_t id, const char *params);
 
 dispatch_entry_t dispatch_table[] = {
-    {NULL, NULL} // sentinel value marks the end of loop iteration over the dispatch table
+    {"initialize", handle_initialize},
+    {NULL, NULL} // sentinel value marks the end of loop iteration over the
+                 // dispatch table
 };
 
 void dispatch_message(const char *method, int32_t id, const char *params) {
-    for (int32_t i = 0; dispatch_table[i].method != NULL; i++) {
-        if (strcmp(method, dispatch_table[i].method) == 0) {
-            dispatch_table[i].handler(id, params);
-            return;
-        }
+  for (int32_t i = 0; dispatch_table[i].method != NULL; i++) {
+    if (strcmp(method, dispatch_table[i].method) == 0) {
+      dispatch_table[i].handler(id, params);
+      return;
     }
+  }
+}
+
+void handle_initialize(int32_t id, const char *params) {
+  const char *response = "{"
+                         "\"jsonrpc\": \"2.0\","
+                         "\"id\": 1,"
+                         "\"result\": {"
+                         "  \"capabilities\": {}"
+                         "}"
+                         "}";
+
+  printf("Content-Length: %zu\r\n", strlen(response));
+  printf("\r\n");
+  printf("%s", response);
+  fflush(stdout);
+
+  // TODO: Parse the initialize request meessage
+  //
+  // TODO: Prepare a response and send it out
 }
